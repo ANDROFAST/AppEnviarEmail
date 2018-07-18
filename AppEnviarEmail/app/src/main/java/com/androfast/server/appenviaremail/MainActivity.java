170dfb1082c4764ec81195c5e31729a0f7b8ff42
AndroFast <cespedessanchezalex@gmail.com>
2018-07-18 00:59:24 -0500

    Enviar email en android studio

---
+++ AppEnviarEmail/app/src/main/java/com/androfast/server/appenviaremail/MainActivity.java
@@ -0,0 +1,47 @@
+package com.androfast.server.appenviaremail;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText para, asunto, mensaje;
+    Button enviar;
+    String sPara, sAsunto, sMensaje;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        para = (EditText) findViewById(R.id.ePara);
+        asunto = (EditText) findViewById(R.id.eAsunto);
+        mensaje = (EditText) findViewById(R.id.eMensaje);
+
+        enviar = (Button) findViewById(R.id.btnEnviar);
+
+        enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sPara = para.getText().toString();
+                sAsunto = asunto.getText().toString();
+                sMensaje = mensaje.getText().toString();
+
+
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{ sPara});
+                email.putExtra(Intent.EXTRA_SUBJECT, sAsunto);
+                email.putExtra(Intent.EXTRA_TEXT, sMensaje);
+
+                //necesita esto para solicitar solo al cliente de correo electrónico
+                email.setType("message/rfc822");
+
+                startActivity(Intent.createChooser(email, "Elija el cliente de correo electrónico :"));
+            }
+        });
+    }
+}
